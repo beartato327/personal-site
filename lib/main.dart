2b31d83f1c08f4b651e5b496514e9fc7ee08bdc1
recap <recapurro327@gmail.com>
2023-05-01 16:13:27 -0500

    Added dynamic navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() {
   runApp(const MyApp());
@@ -22,21 +26,117 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
+    double sw = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-          centerTitle: true,
-          title: const Text("Roberto Capurro | Full Stack Dev")),
-      body: Center(child: Text("Welcome")),
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(60),
+          child: Container(
+              decoration: BoxDecoration(color: Colors.orange),
+              child: AppBarContent(screenWidth: sw,)),
+        ) /* AppBar(
+            centerTitle: true,
+            title: const Text("Roberto Capurro | Full Stack Dev"))*/
+        ,
+        body: ListView(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(5),
+                  child: SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: CircleAvatar(
+                          foregroundImage: NetworkImage(
+                              "https://i.stack.imgur.com/0RUPF.jpg")))),
+              Flexible(
+                  child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: const Text(
+                        "Hi! I am Roberto Capurro and thank you for visiting my site! I can't wait for you to learn more abou tme and my skillset as a developer.",
+                        textAlign: TextAlign.right,
+                      )))
+            ])
+          ],
+        ));
+  }
+}
+
+class AppBarContent extends StatelessWidget {
+  double screenWidth;
+  AppBarContent({super.key, required this.screenWidth});
+  @override
+  Widget build(BuildContext context) {
+    print(screenWidth);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text(
+                'Roberto Capurro | Full Stack Dev',
+                style: screenWidth > 800
+                    ? TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold)
+                    : TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              //const Spacer(),
+              if (screenWidth > 800) ...[
+                Row(children: navBarMenu)
+              ] else ...[
+                IconButton(
+                  icon: const Icon(Icons.menu, size: 20),
+                  color: Colors.white,
+                  onPressed: () {},
+                )
+              ]
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
+
+List<Widget> navBarMenu = [
+  TextButton(
+    child: Text("Welcome", style: TextStyle(color: Colors.white, fontSize: 14)),
+    onPressed: () {},
+  ),
+  TextButton(
+    child:
+    Text("About Me", style: TextStyle(color: Colors.white, fontSize: 14)),
+    onPressed: () {},
+  ),
+  TextButton(
+    child:
+    Text("Education", style: TextStyle(color: Colors.white, fontSize: 14)),
+    onPressed: () {},
+  ),
+  TextButton(
+    child:
+    Text("Experience", style: TextStyle(color: Colors.white, fontSize: 14)),
+    onPressed: () {},
+  ),
+  TextButton(
+    child:
+    Text("Contact Me", style: TextStyle(color: Colors.white, fontSize: 14)),
+    onPressed: () {},
+  ),
+];
